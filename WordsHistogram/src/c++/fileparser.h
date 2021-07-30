cd846219e58c245e9771a1485f8fb3e403d9dec6
GeorgeMerge <BasedGeorge@yandex.ru>
2021-07-30 09:09:39 +0300

    Fix gitignore

---
+++ WordsHistogram/src/c++/fileparser.h
@@ -0,0 +1,27 @@
+#ifndef FILEPARSER_H
+#define FILEPARSER_H
+
+#include <QObject>
+#include <QMap>
+#include <QVariant>
+
+class FileParser : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FileParser(QObject *parent = nullptr);
+    Q_INVOKABLE void parseFileInSeparateThread(QString filePath);
+
+private:
+    void parseFile(QString filePath);
+    QString replacePatternWithWhitespaceConcurrently(QString &data, QRegExp pattern, int substrings);
+    void splitString(const QString &str, int n, QStringList &list);
+    void splitStringSavingWords(const QString &str, int n, QStringList &list);
+    void fillWordsCountConcurrently(QString &data, QVariantMap &map, int substrings);
+
+signals:
+    void progressChanged(qreal value);
+    void fileParsed(QVariantMap resultMap);
+};
+
+#endif // FILEPARSER_H
